4c5d7bfd40ebd27ed5a9e6344acef48907c692ec
aramisdev <aods@ic.ufal.br>
2022-03-25 21:43:08 -0300

    Componentes em React

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Componentes/Header';
+import Footer from './Componentes/Footer';
+import HomePage from './Componentes/HomePage';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <HomePage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
